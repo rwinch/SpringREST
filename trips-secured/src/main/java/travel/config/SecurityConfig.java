2d1890d47b5a1113617086b8ca000fc3cce0e245
Rob Winch <rwinch@gopivotal.com>
2014-10-02 15:21:01 -0500

    Enable CSRF
    We should not disable CSRF protection. This commit enables CSRF.
    
    Some commentary on the configuration as it was.
    
    1) In order to get the CSRF automatically included we need to use @EnableWebMvcSecurity (notice Mvc). This
    provides the RequestDataValueProcessor used by Thymeleaf and the Spring taglibs to automatically include
    the hidden input with the CSRF token.
    
    2) The configuraton was:
    
    http
        ...
        .csrf()
            .requireCsrfProtectionMatcher(new AntPathRequestMatcher("/oauth/authorize")).disable()
    
    The configuration is probably better read as:
    
    http
       ...
       .csrf()
         .requireCsrfProtectionMatcher(new AntPathRequestMatcher("/oauth/authorize"))
         .disable()
    
    So what is it actually doing?
    
    a) The requireCsrfProtectionMatcher statement says CSRF will only be applied for the RequestMatcher passed in.
    b) The disable statement says that CSRF will be disabled
    
    This means that the first statement actually did nothing since we disable all of CSRF afterwards. This shows
    the importance of spacing so that we read the configuration. Note as the indentation is in the example above
    it demonstrates that both requireCsrfProtectionMatcher and disable impact csrf. The way expressed previously
    it appeared that disable impacted the requireCsrfProtectionMatcher.
    
    How do you know which is which? The value returned from requireCsrfProtectionMatcher is the same type as what
    is returned by csrf.
    
    In hindsight is this a bit confusing? Yes...are we stuck with it...yes. I have seen simpilar problems with
    others and the only comfort upon the decision of the DSL is that my DSL has the same problems as any language.
    The reality is spacing matters in any language, including this DSL. For exmaple I could write
    
    at tributes
    
    or
    
    attributes
    
    They have different meanings based on the spacing.

--- trips-secured/src/main/java/travel/config/SecurityConfig.java
+++ trips-secured/src/main/java/travel/config/SecurityConfig.java
@@ -4,12 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebMvcSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -23,10 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .loginProcessingUrl("/login")
           .and()
               .exceptionHandling()
-                  .accessDeniedPage("/login?auth_error=true")
-          .and()
-              .csrf()
-                  .requireCsrfProtectionMatcher(new AntPathRequestMatcher("/oauth/authorize")).disable();
+                  .accessDeniedPage("/login?auth_error=true");
     }
 
     @Autowired
